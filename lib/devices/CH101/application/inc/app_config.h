023d4838891098c3589b6340dd17a588dfdc3198
po-nokonoko <popopo20190807@gmail.com>
2025-08-13 20:39:20 +0800

    Modification: define non-blocking I/Q reading

--- lib/devices/CH101/application/inc/app_config.h
+++ lib/devices/CH101/application/inc/app_config.h
@@ -99,6 +99,8 @@
  * FREERUN 會好一點 */
 /* WARNING: 換成用 hand_config.h 配置模式 */
 #define CHIRP_FIRST_SENSOR_MODE CH_MODE_FREERUN
+// Test the extent of interuption... 
+//#define CHIRP_FIRST_SENSOR_MODE CH_MODE_TRIGGERED_TX_RX
 #define CHIRP_OTHER_SENSOR_MODE CH_MODE_TRIGGERED_RX_ONLY
 
 /* Maximum detection range for the sensor
@@ -224,11 +226,11 @@
  * from the application and analyze it in a spreadsheet or other program.
  */
 
-// #define READ_IQ_DATA			/* uncomment this line to readout I/Q
+#define READ_IQ_DATA			/* uncomment this line to readout I/Q
 // data */
 
-#define READ_IQ_BLOCKING /* use blocking mode when reading I/Q */
-// #define READ_IQ_NONBLOCKING 	/* use non-blocking mode when reading I/Q */
+// #define READ_IQ_BLOCKING /* use blocking mode when reading I/Q */
+#define READ_IQ_NONBLOCKING 	/* use non-blocking mode when reading I/Q */
 
 // #define OUTPUT_IQ_DATA_CSV	/* uncomment to output I/Q data in CSV format */
 
